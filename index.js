6e1f63dd185543d3fd6d87978a241bc7f4e84181
Willigers1 <ronowilligers@gmail.com>
2024-07-01 23:44:29 +0300

    added a new file in html and js

---
+++ index.js
@@ -0,0 +1,50 @@
+function calculateGrade() {
+	const marks = parseInt(document.getElementById("marks").value);
+	let grade;
+	// const gradeElement= document.getElementById("garde");{
+	// 	if (isNaN(marks) || marks < 0 || marks > 100) {
+	// 	gradeElement.textContent =
+	// 		"Error: Please enter a valid number between 0 and 100.";
+	// 	return;
+	// }
+
+	if (marks > 79) {
+		grade = "A";
+	} else if (marks >= 60 && marks <= 79) {
+		grade = "B";
+	} else if (marks >= 50 && marks <= 59) {
+		grade = "C";
+	} else if (marks >= 40 && marks <= 49) {
+		grade = "D";
+	} else {
+		grade = "E";
+	}
+
+	document.getElementById("grade").textContent = `Grade: ${grade}`;
+}
+
+
+
+
+
+
+// SPEED DETECTOR
+function calculate() {
+    // get the value of an input field with an id of speed using the document.getElementById() method
+    // value is then parsed as an integer using parseInt() and assigned to a variable named speed
+    const speed = parseInt(document.getElementById('speed').value);
+    let points = 0;
+    //checks whether the speed is less than 70. If it is, the text content of an HTML element with an id of result is set to "Ok
+    if (speed < 70) {
+      document.getElementById('result').textContent = 'Ok';
+    } else {
+    // driver receives 1 point for every 5 km/h over the speed limit of 70 km/h 
+      points = Math.floor((speed - 70) / 5);
+    //checks whether the number of points exceeds 12. If it does, the text content of the HTML element with an id of result is set to "License suspended"   
+      if (points > 12) {
+        document.getElementById('result').textContent = 'License suspended';
+      } else {
+        document.getElementById('result').textContent = 'Points: ' + points;
+      }
+    }
+  }
